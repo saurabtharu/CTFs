2285ca73bdeb5d391baa6032408ffa8bcf01ddb5
Saurab Tharu <hi@saurabtharu.com.np>
2022-07-23 13:20:41 +0545

    level 63

---
+++ pwn.college/embryoio/level0063.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+void pwncollege(){
+	char *command[] = {"/challenge/embryoio_level63",0};
+	char *bin_file = command[0];
+	
+	if(fork()==0){
+		execvp(bin_file,command);
+	}else{
+		wait(NULL);
+	}
+}
+
+int main(){
+	pwncollege();
+}
+
+
+/*
+./a.out | rev | rev
+*/
